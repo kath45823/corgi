f9c58755ab7496cb81d08dcde51e5efb9af7e737
kath <kath45823@gmail.com>
2026-03-28 17:24:25 -0400

    feat: add titles to SVG icons

--- lib/main.ts
+++ lib/main.ts
@@ -4,5 +4,5 @@ export * from "@untitledui/icons";
 export { Banner } from "./components/Banner";
 export { Button } from "./components/Button";
 export { Checkbox } from "./components/Checkbox";
-export { SponsorCarousel } from "./components/SponsorCarousel";
 export { Dropdown } from "./components/Dropdown";
+export { SponsorCarousel } from "./components/SponsorCarousel";
